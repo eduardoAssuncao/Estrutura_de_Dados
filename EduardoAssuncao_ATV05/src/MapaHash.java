e5673309cfac31621853c39a755bc93179477746
Eduardo (Kenny8855) Assunção <39742355+eduardoAssuncao@users.noreply.github.com>
2021-12-27 12:15:30 -0300

    Add files via upload

---
+++ EduardoAssuncao_ATV05/src/MapaHash.java
@@ -0,0 +1,62 @@
+public class MapaHash {
+    private Registro valores[];
+
+    public MapaHash(){
+        valores = new Registro[100];
+    }
+
+    public void put (Registro registro){
+        int posicao = registro.hashCode();
+        if(valores[posicao] == null){
+            valores[posicao] = registro;
+            System.out.println("=======Posicao vazia!! Novo Registro");
+        }else{
+            System.out.println("Ops... Colisao >>>");
+            Registro reg = valores[posicao];
+
+            // especial para tratar o primeiro elemento
+            if (reg.getKey() == registro.getKey()){ // o registro que quero inseir é o mesmo que
+                reg.setValue(reg.getValue()); // apenas substituo o valor
+                System.out.println("É o primeiro Registro Existente - atualizando...");
+                return;
+            }
+            // se ele tá no meio da lista
+            while (reg.getProximo() != null){
+                if (reg.getKey() == registro.getKey()){ // o registro que quero inseir é o mesmo que
+                    reg.setValue(registro.getValue()); // apenas substituo o valor
+                    System.out.println("Tá no meio da lista - Registro Existente - atualizando");
+                    return;
+                }
+                reg = reg.getProximo();
+            }
+            //se ele é o último da lista
+            //if (reg.getProximo() == null){
+                if(reg.getKey() == registro.getKey()){
+                    reg.setValue(registro.getValue());
+                    System.out.println("É o último! Registro Existente - atualizando");
+                    return;
+                }
+            //}
+            reg.setProximo(registro); // coloquei o novo registro na última posição
+            System.out.println("Novo Registro!");
+        }
+    }
+
+    public Registro get (int key){
+        Registro r = new Registro();
+        r.setKey(key);
+        int posicao = r.hashCode();
+        Registro resultado = valores[posicao]; // tento encontrar o registro
+        if (resultado != null && resultado.getKey() == key){ // é o que eu tô proxutando?
+            return resultado;
+        }else{
+            while(resultado != null){ // enquanto tiverr gente na lista...
+                resultado = resultado.getProximo(); // vou para o próximo
+                if (resultado != null && resultado.getKey() == key){ // é o cara?
+                    return resultado; // se for, retorno ele
+                }
+            }
+        }
+        return null;
+    }
+}
